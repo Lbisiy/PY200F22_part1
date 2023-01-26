7af71bb7dd66b9a11ac6c90966387d60216e2c42
Marat <maratsafin.let@gmail.com>
2023-01-26 23:11:38 +0100

    Lab3

--- Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
+++ Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
@@ -1,8 +1,16 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self.name_ = name
+        self.author_ = author
+
+    @property
+    def name(self):
+        return self.name_
+
+    @property
+    def author(self):
+        return self.author_
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -11,21 +19,33 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-class PaperBook:
-    def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
+class PaperBook(Book):
+    def __init__(self, pages: int):
+        super(Book, self).__init__()
         self.pages = pages
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    def init_pages(self, pages: int):
+        if not isinstance(pages, int):
+            raise TypeError('Атрибут pages должен быть типа int')
+        if pages < 1:
+            raise ValueError('Атрибут pages должен быть положительным')
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
 
 
-class AudioBook:
-    def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
+class AudioBook(Book):
+    def __init__(self, duration: float):
+        super(Book, self).__init__()
+        self.duration = self.init_duration(duration)
+
+    def init_duration(self, duration: float):
+        if not isinstance(duration, float):
+            raise TypeError('Атрибут duration должен быть типа float')
+        if duration <= 0:
+            raise ValueError('Атрибут duration должен быть положительным')
         self.duration = duration
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
